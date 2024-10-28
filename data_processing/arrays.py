41375f8b296993bc00e824cec84057a3efa66916
aleksandr4726 <aleksandr.13151719@gmail.com>
2024-10-28 19:26:41 +0300

    Удалил /idea из documentation

--- data_processing/arrays.py
+++
@@ -1,9 +0,0 @@
-import math
-def avg(array):
-    avg =sum(array)/ len(array)
-    x = [(i - avg) ** 2 for i in array]
-    y=sum(x)
-    z=math.sqrt(y / (len(array) * (len(array) - 1)))
-  #  return(avg,z)
-    return (["{:.5g}".format(avg),"{:.3g}".format(z)])
-print(avg([0.0001,0.0001,0.0001,0.0001,0.0001,0.0001]))
